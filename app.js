c0b16c8911f18865df67dc1dd9bb1ffc9d745a5d
niiwade <niilantewade77@gmail.com>
2021-04-01 06:42:29 +0000

    defining the dependencies to used in the app

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 
 // Routes
-
+dotenv.config({ path: 'config.env' });
 app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
+
+
+// log requests
+app.use(morgan('tiny'));
+
+//body parser request 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+//app.set("routes", path.resolve(__dirname,"routes"));
+
+
+
 
 app.listen(PORT, console.log(`Server started on port: ${PORT}`));
