dd4209983decb94a2740074a89af6ee048703abf
niiwade <niilantewade77@gmail.com>
2021-04-03 09:04:39 +0000

    set ejs view engine

--- app.js
+++ app.js
@@ -2,11 +2,25 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 
 const app = express();
 
-dotenv.config({ path: 'config.env' })
+dotenv.config({ path: 'config.env' });
 const PORT = process.env.PORT || 8000;
+
+
+
+// logs
+app.use(morgan('tiny'));
+
+//parser requests
+app.use(bodyparser.urlencoded({ extended: true }));
+
+
+//ejs view engine
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.send("Hello");
